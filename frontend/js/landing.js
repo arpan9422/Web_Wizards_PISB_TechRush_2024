6118bdcbd99db3f39598675771dcddfa33e6f2a3
arpan9422 <arpanagrawal552@gmail.com>
2024-07-19 00:05:04 +0530

    landing page halfway done

---
+++ frontend/js/landing.js
@@ -0,0 +1,4 @@
+document.getElementById("close").addEventListener('click', function(){
+            console.log(document.getElementById("alert"))
+            document.getElementById("alert").style.display = "none";
+        });
