05c55832821149d8799e949d55ce913b8e527d19
arpan9422 <arpanagrawal552@gmail.com>
2024-07-18 19:25:23 +0530

    dashboardjs named changed to dashboardPopup.js

---
+++ frontend/js/footer.js
@@ -0,0 +1,148 @@
+document.getElementById("footer").innerHTML = `
+<footer class="ps-12 border-t-2 border-gray-200 bg-white">
+  <section class="relative overflow-hidden py-10">
+    <div class="relative z-10 mx-auto max-w-7xl px-4">
+      <div class="-m-6 flex flex-wrap">
+        <div class="w-full p-6 md:w-1/2 lg:w-5/12">
+          <div class="flex h-full flex-col justify-between">
+            <div class="mb-4 inline-flex items-center">
+              <img
+                width="40"
+                height="46"
+                viewBox="0 0 50 56"
+                fill="none"
+                src="./img/Logo.png"
+                alt="Finatic Logo"
+              />
+              <span class="ml-4 text-xl font-bold">Finatic</span>
+            </div>
+            <div>
+              <p class="mb-4 text-lg font-medium">
+                Empowering Financing with Analytics
+              </p>
+              <p class="text-sm text-gray-600">
+                © Copyright 2024. All Rights Reserved by Finatic.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full p-6 md:w-1/2 lg:w-2/12">
+          <div class="h-full">
+            <h3 class="tracking-px mb-9 text-xs font-semibold uppercase text-gray-500">
+              Company
+            </h3>
+            <ul>
+              <li class="mb-4">
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Features
+                </a>
+              </li>
+              <li class="mb-4">
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Pricing
+                </a>
+              </li>
+              <li class="mb-4">
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Affiliate Program
+                </a>
+              </li>
+              <li>
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Press Kit
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="w-full p-6 md:w-1/2 lg:w-2/12">
+          <div class="h-full">
+            <h3 class="tracking-px mb-9 text-xs font-semibold uppercase text-gray-500">
+              Support
+            </h3>
+            <ul>
+              <li class="mb-4">
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Account
+                </a>
+              </li>
+              <li class="mb-4">
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Help
+                </a>
+              </li>
+              <li class="mb-4">
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Contact Us
+                </a>
+              </li>
+              <li>
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Customer Support
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="w-full p-6 md:w-1/2 lg:w-3/12">
+          <div class="h-full">
+            <h3 class="tracking-px mb-9 text-xs font-semibold uppercase text-gray-500">
+              Legals
+            </h3>
+            <ul>
+              <li class="mb-4">
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Terms & Conditions
+                </a>
+              </li>
+              <li class="mb-4">
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a
+                  class="text-base font-medium text-gray-900 hover:text-gray-700"
+                  href="#"
+                >
+                  Licensing
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</footer>
+`;
