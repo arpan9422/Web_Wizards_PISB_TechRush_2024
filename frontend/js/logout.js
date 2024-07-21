98200a2f0738af750236215e3842f755a436f9f8
Hrushikesh Patwardhan <hrushikeshp13@gmail.com>
2024-07-21 12:49:30 +0530

    fixed logout

--- frontend/js/logout.js
+++ frontend/js/logout.js
@@ -1,10 +1,15 @@
 
 const server_addr = "http://localhost:3000/user/Logout"
 
+var delete_cookie = function(name) {
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
+
 function logout() {
     // Clear the JWT token from local storage or session storage
     localStorage.removeItem('auth_tok');
     sessionStorage.removeItem('auth_tok');
+    delete_cookie('auth_tok')
 
     // Redirect to the login page
     window.location.href = '/landing.html';
