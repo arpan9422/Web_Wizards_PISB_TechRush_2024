526c83b39b386297ccbfeef3eb3d0ca434c3f8d2
Hrushikesh Patwardhan <hrushikeshp13@gmail.com>
2024-07-21 23:01:22 +0530

    add node modules back (again)

--- frontend/js/analytics.mjs
+++ frontend/js/analytics.mjs
@@ -19,4 +19,25 @@ export async function get_analytics(scope)
     return json_body;
 }
 
+export async function get_monthly_analytics(scope)
+{
+    let result =  await fetch("/user/fetchMonthlyAnalytics", {
+	method: "POST",
+	
+	headers: {
+	    "Content-Type": "application/json"
+	},
+
+	body: JSON.stringify({
+	    scope: scope
+	})
+    });
+
+    console.log(result);
+    
+    let json_body = await result.json();
+
+    return json_body;
+}
+
 
